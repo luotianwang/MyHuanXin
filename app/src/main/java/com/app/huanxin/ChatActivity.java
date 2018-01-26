562e72cc15017f802610499e70df653878ff816c
luomengcheng <luomengcheng@mobase.cn>
2018-01-26 10:37:55 +0800

    实现单聊功能！

--- app/src/main/java/com/app/huanxin/ChatActivity.java
+++ app/src/main/java/com/app/huanxin/ChatActivity.java
@@ -1,11 +1,15 @@
 package com.app.huanxin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
+import com.app.huanxin.runtimepermissions.PermissionsManager;
 import com.app.huanxin.util.Constant;
 import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.ui.EaseBaseActivity;
 import com.hyphenate.easeui.ui.EaseChatFragment;
+import com.hyphenate.util.EasyUtils;
 
 /**
  * Created by apple on 2018/1/25.
@@ -13,24 +17,63 @@ import com.hyphenate.easeui.ui.EaseChatFragment;
 
 public class ChatActivity extends EaseBaseActivity {
 
-    public static String toUserName = "";
+    public String toChatUsername = "";
+    public static ChatActivity activityInstance;
 
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         setContentView(R.layout.activity_chat);
-
+        activityInstance = this;
         String userName = getIntent().getStringExtra(Constant.EXTRA_USER_ID);
-        toUserName = userName;
+        toChatUsername = userName;
         //new出EaseChatFragment或其子类的实例
         EaseChatFragment chatFragment = new EaseChatFragment();
         //传入参数
         Bundle args = new Bundle();
-        args.putInt(EaseConstant.EXTRA_CHAT_TYPE, EaseConstant.CHATTYPE_GROUP);
+//        args.putInt(EaseConstant.EXTRA_CHAT_TYPE, EaseConstant.CHATTYPE_GROUP);
         args.putString(EaseConstant.EXTRA_USER_ID, userName);
         chatFragment.setArguments(args);
         getSupportFragmentManager().beginTransaction().add(R.id.layout_content, chatFragment).commit();
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        activityInstance = null;
+    }
+
+    public String getToChatUsername() {
+        return toChatUsername;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // make sure only one chat activity is opened
+        String username = intent.getStringExtra(Constant.EXTRA_USER_ID);
+        if (toChatUsername.equals(username))
+            super.onNewIntent(intent);
+        else {
+            finish();
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (EasyUtils.isSingleActivity(this)) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
     }
 
 }
--- app/src/main/java/com/app/huanxin/HomeActivity.java
+++ app/src/main/java/com/app/huanxin/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.app.huanxin;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
@@ -10,6 +12,8 @@ import android.widget.Toast;
 
 import com.app.huanxin.db.InviteMessgeDao;
 import com.app.huanxin.domain.InviteMessage;
+import com.app.huanxin.runtimepermissions.PermissionsManager;
+import com.app.huanxin.runtimepermissions.PermissionsResultAction;
 import com.app.huanxin.util.HXHelper;
 import com.hyphenate.EMContactListener;
 import com.hyphenate.EMMessageListener;
@@ -35,6 +39,7 @@ public class HomeActivity extends EaseBaseActivity implements View.OnClickListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions();
         initview();
         changeFragement(getSupportFragmentManager().beginTransaction(), new ConversationFragment());
     }
@@ -77,7 +82,6 @@ public class HomeActivity extends EaseBaseActivity implements View.OnClickListen
                     Toast.makeText(getApplicationContext(), "收到消息", Toast.LENGTH_LONG).show();
                 }
             });
-
 
             for (EMMessage message : messages) {
                 String username = null;
@@ -271,5 +275,26 @@ public class HomeActivity extends EaseBaseActivity implements View.OnClickListen
                 changeFragement(transaction, contactListFragment);
                 break;
         }
+    }
+
+    @TargetApi(23)
+    private void requestPermissions() {
+        PermissionsManager.getInstance().requestAllManifestPermissionsIfNecessary(this, new PermissionsResultAction() {
+            @Override
+            public void onGranted() {
+//				Toast.makeText(MainActivity.this, "All permissions have been granted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDenied(String permission) {
+                //Toast.makeText(MainActivity.this, "Permission " + permission + " has been denied", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
     }
 }
